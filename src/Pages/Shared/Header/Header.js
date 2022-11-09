17df3fc0e1c8cad33913cb79a1e41d72091b9564
shourov hasan <shourovhasan172@gmail.com>
2022-11-09 09:12:37 +0600

    header section private button added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -13,11 +13,17 @@ const Header = () => {
         .catch(error => console.error(error))
       }
 
-  const menuItems = <>
+  const menuItems1 = <>
   <li className='font-semibold'><Link to='/'>Home</Link></li>
   <li className='font-semibold'><Link to='/blogs'>Blogs</Link></li>
-  <li className='font-semibold'><Link to='/login'>Login</Link></li>
-</>
+  <li className='font-semibold'><Link to='/myreview'>My Review</Link></li>
+  <li className='font-semibold'><Link to='/addservice'>Add Service</Link></li>
+    </>
+
+  const menuItems2 = <>
+  <li className='font-semibold'><Link to='/'>Home</Link></li>
+  <li className='font-semibold'><Link to='/blogs'>Blogs</Link></li>
+    </>
 
   return (
     <div className="navbar h-20 mb-12 pt-12 bg-base-100">
@@ -27,7 +33,13 @@ const Header = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                {menuItems}
+                {
+                    user?.uid ?
+                    <>{menuItems1}</>
+                    :
+                    <>{menuItems2}</>
+                }
+                
             </ul>
         </div>
         <Link to="/" className="btn btn-ghost normal-case text-xl">
@@ -36,7 +48,12 @@ const Header = () => {
     </div>
     <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal p-0">
-            {menuItems}
+            {
+                user?.uid ?
+                <>{menuItems1}</>
+                :
+                 <>{menuItems2}</>
+            }
         </ul>
     </div>
     <div className="navbar-end">
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,8 +1,10 @@
 import Main from "../../Layout/Main";
+import AddService from "../../Pages/AddService/AddService";
 import Blogs from "../../Pages/Blogs/Blogs";
 import Gallery from "../../Pages/Gallery/Gallery";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
+import MyReview from "../../Pages/MyReview/MyReview";
 import ServiceDetails from "../../Pages/Services/ServiceDetails/ServiceDetails";
 import Services from "../../Pages/Services/Services";
 import SignUp from "../../Pages/SignUp/SignUp";
@@ -42,6 +44,14 @@ const router = createBrowserRouter([
           path: '/servicedetails/:id', 
           element: <ServiceDetails/>,
           loader: ({params})=> fetch(`http://localhost:5000/services/${params.id}`)
+        },
+        {
+          path: '/myreview', 
+          element: <MyReview/>
+        },
+        {
+          path: '/addservice', 
+          element: <AddService/>
         },
         
       ]
